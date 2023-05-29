7287a9c18c336dd7420f6a525a2032ed9cd9a884
Diego França <diego.franca92@gmail.com>
2023-05-29 19:14:20 -0300

    include swagger documentation

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,15 +1,19 @@
-import express from "express";
-import { routes } from "./routes";
-import cors from 'cors';
+import express from 'express'
+import { routes } from './routes'
+import cors from 'cors'
+import swaggerDefs from './swagger.json'
+import swaggerUI from 'swagger-ui-express'
 
-const app = express();
+const app = express()
 
-app.use(cors());
+app.use(cors())
 
-app.use(express.json());
+app.use(express.json())
 
 app.use(routes)
 
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDefs))
+
 app.listen(3333, () => {
-  console.log("Express server listening on port 3333");
-});
+  console.log('Express server listening on port 3333')
+})
