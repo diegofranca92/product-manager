138528e9e5a71cac243b85be3fb45ddcc09e28e1
Diego França <diego.franca92@gmail.com>
2023-07-04 22:07:47 +0000

    create Sign page and menu itens

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,12 @@ import Link from "next/link";
 
 export default function SignIn() {
   return (
-    <main>
-      <Link href="/home">Home </Link>
-      <Link href="/product">Produto</Link>
+    <main className="flex align-center justify-center">
+      <form className="shadow p-4 gap-4 flex flex-col w-80">
+        <input type="email" className="p-2 rounded border" placeholder="Usuário" />
+        <input type="password" className="p-2 rounded border" placeholder="Senha" />
+        <input type="submit" className="p-2 rounded bg-sky-500 hover:bg-sky-600 hover:cursor-pointer text-white" />
+      </form>
     </main>
   );
 }
