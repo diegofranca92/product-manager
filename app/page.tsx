12d825382d3b6c939bc9b8e7c8d78e8c91fb3d37
Diego França <diego.franca92@gmail.com>
2023-07-04 22:14:34 +0000

    Finish SignPage

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,12 @@ import Link from "next/link";
 
 export default function SignIn() {
   return (
-    <main className="flex align-center justify-center">
+    <main className="flex items-center justify-center h-screen">
       <form className="shadow p-4 gap-4 flex flex-col w-80">
-        <input type="email" className="p-2 rounded border" placeholder="Usuário" />
-        <input type="password" className="p-2 rounded border" placeholder="Senha" />
-        <input type="submit" className="p-2 rounded bg-sky-500 hover:bg-sky-600 hover:cursor-pointer text-white" />
+        <h1 className="text-center text-xl">Login</h1>
+        <input type="email" className="p-2 rounded border" placeholder="Usuário" required/>
+        <input type="password" className="p-2 rounded border" placeholder="Senha" required/>
+        <input type="submit" value="Fazer Login" className="p-2 rounded bg-sky-500 hover:bg-sky-600 hover:cursor-pointer text-white" />
       </form>
     </main>
   );
